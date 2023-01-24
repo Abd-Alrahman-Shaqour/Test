c23a76e17aaf4833559a4e4e7aaac31e2275ba2a
AbdAlrahman Shaqour <abdmaher01@gmail.com>
2023-01-24 14:07:49 +0300

    adasd

--- My project (8)/Assets/dar.cs
+++ My project (8)/Assets/dar.cs
@@ -7,12 +7,7 @@ public class dar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string idk;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
